41cbf473ce158f1c40146fdb3baf5fb0e8fcb9ca
KOO612 <ksm99007@gmail.com>
2024-03-08 18:00:46 +0900

    0308

--- fastcampus/part04/server.js
+++ fastcampus/part04/server.js
@@ -3,9 +3,20 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Tpye': 'text/plain',
+    // 콘텐츠 타입을 텍스트로
+    // 'Content-Tpye': 'text/plain',
+
+    // 콘텐츠 타입을 js 오브젝트로
+    'Content-Type': 'application/json',
   });
-  res.end('Hello!');
+  // res.end('Hello!');
+  res.end(
+    // end 메소드는 문자열을 받기때문에 json으로 변환
+    JSON.stringify({
+      a: 'a',
+      b: 'b',
+    })
+  );
 });
 
 server.listen(port, () => {
---
+++ fastcampus/part04/server2.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/home') {
+    req.on('data', (data) => {
+      console.log('data', data);
+      const stringfiedData = data.toString();
+      console.log('stringfiedData', stringfiedData);
+      Object.assign(dataObject, JSON.parse(stringfiedData));
+    });
+  } else {
+    if (req.url === '/home') {
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({
+          a: 'a',
+          b: 'b',
+        })
+      );
+    } else if (req.url === '/about') {
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<body>');
+      res.write('<h1>About page</h1>');
+      res.write('</body>');
+      res.write('</html>');
+    } else {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
+});
+
+server.listen(port, () => {
+  console.log(`시작 ${port}`);
+});
